5f6a983439ed0801de220861a25c02222343f412
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-08 19:56:26 +0530

    Dependent Function Type

---
+++ src/main/scala/com/allevite/scala3Reference/chapt5_DependentFunctionType/DependentFunctionType.scala
@@ -0,0 +1,18 @@
+package com.allevite.scala3Reference.chapt5_DependentFunctionType
+
+object DependentFunctionType:
+  // A dependent function type is a function type whose result depends on function's parameters
+
+  trait Entry:
+    type Key
+    def key: Key
+
+  def extractKey(e: Entry): e.Key = e.key   // a dependent method
+
+  val df: (e: Entry) => e.Key = extractKey // a dependent function type
+
+  class MyEntry extends Entry:
+    override type Key = String
+    override def key: String = "secrete key"
+
+  @main def MAIN(): Unit = println(df(new MyEntry()))
