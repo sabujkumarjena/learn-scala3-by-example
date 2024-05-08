070da5d431f0725e90b6249fa5b9e22c6778d8ba
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-08 21:26:12 +0530

    Polymorphic function types

---
+++ src/main/scala/com/allevite/scala3Reference/chapt6_PolymorphicFunctionTypes/PolymorphicFunctionType.scala
@@ -0,0 +1,16 @@
+package com.allevite.scala3Reference.chapt6_PolymorphicFunctionTypes
+
+object PolymorphicFunctionType :
+  //A polymorphic method
+  def foo[A](xs: List[A]): List[A] = xs.reverse
+
+  //A polymorphic function
+  val bar: [A] => List[A] => List[A] =
+    [A] => (xs: List[A]) => xs.reverse
+
+  val toMap: [K,V] => K => V => Map[K,V] =
+  [K,V] => (key: K) => (value: V) => Map(key -> value)
+
+  @main def MAIN(): Unit = ()
+    println(bar(List(1,2,3)))
+    println(toMap("price")(35))
